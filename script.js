a95e78ecffefc87621a87ca7cdc1876412b47892
Alief Zulvan <depanb187@gmail.com>
2022-07-31 16:15:15 +0700

    nambahin kelas darkmode tapi blum fix

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-var rr = document.querySelectorAll('.gam');
+// darkmode toggle
+const darkToggle = document.querySelector('#darkmode');
+const html = document.querySelector('html');
 
-function hapus() {}
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+// pindah toglle mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
